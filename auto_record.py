2acda46503aa1044b238e6496d7c8ec500799a14
Gerardo Gomez <olikase24@gmail.com>
2023-10-13 10:14:00 -0600

    Add prints

--- auto_record.py
+++ auto_record.py
@@ -7,32 +7,34 @@ def timer(t: float):
     for i in range(int(t)):
         print(f"Tiempo restante: {t - i} segundos")
         time.sleep(1)
-        # Sonido de alerta antes de los ultimos 3 segundos cada segundo
-        if i >= t - 3:
-            winsound.Beep(1000, 1000)
-            time.sleep(1)
 
 # Tiempo de espera antes de empezar a grabar
-timer(10)
+print("Acomoda el mouse en la posición deseada")
+timer(20)
 
 # Toma la posición del mouse
 x, y = pyautogui.position()
-print(x, y)
+print(f"Posicion grabada {x}, {y}")
 
 # Da click en la posición del mouse
 pyautogui.click(x, y)
 
 # Tiempo de espera antes de iniciar las pruebas
+print("Inicia las pruebas en 10 segundos")
 timer(10)
 
 # Inicia las pruebas
+print("Inicia las pruebas")
 timer(20)
 
 # Tiempo de descanso entre pruebas
-timer(10)
+print("Descansa 20 segundos")
+timer(20)
 
 # Inicia las pruebas
+print("Inicia las pruebas")
 timer(20)
 
 # Click en la posición del mouse
+print("Fin de la grabación")
 pyautogui.click(x, y)
